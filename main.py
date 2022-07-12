8988ec6e789c09a06d18a81903105a38b3425e96
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-07-12 00:13:15 -0500

    fixed bugs

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from crypt import methods
 from urllib import response
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import csrf as token_generator
+from flask_wtf.csrf import CSRFProtect
 
 import json
 
+
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
+app.secret_key = "12345678"
+# print(csrf._get_csrf_token())
 
 
 
@@ -14,14 +21,14 @@ class Singleton:
     array = []
 
     @staticmethod
-    def getInstance():
+    def get_instance():
         if Singleton._instance == None:
             Singleton()
         return Singleton._instance
     
     def __init__(self):
         if Singleton._instance != None:
-            raise Exception('This class is a singleton!')
+            raise RuntimeError('This class is a singleton!')
         else:
             Singleton._instance = self
 
@@ -30,9 +37,15 @@ class Singleton:
     # def __init__(self, name):
         # self.name  = name
         # self.count = 0
+
 @app.route('/')
 def index():
-    return 'hola'
+    return "hola" 
+
+@app.route('/get-token')
+def token():
+    response = {"token":token_generator.generate_csrf() }
+    return jsonify(response)
 
 @app.route('/hola')
 def hola():
@@ -52,7 +65,7 @@ def adios():
 
 @app.route('/singleton',methods=['POST'])
 def singleton():
-    s = Singleton.getInstance()
+    s = Singleton.get_instance()
     print(int(request.get_json()['data']))
     s.array.append(int(request.get_json()['data']))
     response = {'len':len(s.array)}
--- test_f.py
+++ test_f.py
@@ -1,5 +1,8 @@
+from wsgiref import headers
+
+from regex import R
 from main import app
-from main import Singleton
+from main import Singleton,csrf
 import pytest
 
 
@@ -17,53 +20,60 @@ def test1(client):
 
     assert response.data.decode('utf-8') == 'hola'
     assert response.status_code == 200
-    # assert 1 == 1
 
 def test2(client):
+    """Start with a blank database."""
+
+    response = client.get('/get-token')
+    json_data = response.get_json()
+
+    assert 'token' in json_data 
+    assert type(json_data['token']) == type("")
+
+def test3(client):
     """Start with a blank database."""
 
     response = client.get('/hola')
     assert response.json['vamos a ver']  == 3
     # assert 1 == 1
 
-def test3(client):
-    """Start with a blank database."""
-
-    response = client.post('/adios',json = {
-        "2": 'no',
-    })
-    assert response.json['no vamos a ver']  == 'si' 
-
 def test4(client):
     """Start with a blank database."""
 
+    response = client.get('/get-token')
+
     response = client.post('/adios',json = {
-        "2": 'si',
-    })
-    assert response.json['no vamos a ver']  == 'no' 
+        "2": 'no',
+    },headers={'X-CSRFToken':response.get_json()['token'],"Content-Type":'application/json'})
+    assert response.json['no vamos a ver']  == 'si' 
 
 def test5(client):
     """Start with a blank database."""
 
+    response = client.get('/get-token')
+    response = client.post('/adios',json = {
+        "2": 'si',
+    },headers={'X-CSRFToken':response.get_json()['token'],"Content-Type":'application/json'})
+    assert response.json['no vamos a ver']  == 'no' 
+
+def test6(client):
+    """Start with a blank database."""
+
+    response1 = client.get('/get-token')
     response = None
     data = ['1324','31412','23141']
     for i in data: 
         response = client.post('/singleton',json = {
             "data": i,
-        })
+        },headers={'X-CSRFToken':response1.get_json()['token'],"Content-Type":'application/json'})
     assert int(response.json['len'])  == len(data) 
 
 
-def test5(client):
+def test7(client):
     """Start with a blank database."""
-    s =Singleton()
-    assert s == Singleton.getInstance()
+    Singleton._instance = None
+    s = Singleton()
+    assert s == Singleton.get_instance()
 
 
 
-# def test5(client):
-    # """Start with a blank database."""
-    # s =Singleton()
-    # with pytest.raises(Exception) as e:
-        # Singleton()
-    # assert e.value.args[0] == 'This class is a singleton!' 
